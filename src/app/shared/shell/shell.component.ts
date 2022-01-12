7f3eae293ff30312eb348df0006277e991f7b900
darkeaee <jvcq13@gmail.com>
2022-01-12 17:53:52 -0300

    initial commit + app navigation shell

---
+++ src/app/shared/shell/shell.component.ts
@@ -0,0 +1,22 @@
+import { Component} from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs';
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss']
+})
+export class ShellComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointOberserver.observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  
+
+  constructor(private breakpointOberserver: BreakpointObserver) { }
+
+}
