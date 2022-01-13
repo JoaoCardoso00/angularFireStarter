2d89c5b22924dcbdbae71de950e02b121c93245c
darkeaee <jvcq13@gmail.com>
2022-01-13 17:40:35 -0300

    added firebase to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -13,7 +17,10 @@ import { HomePageComponent } from './home-page/home-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'angularkanban-e7ae9',
+    appId: '1:302926177904:web:cd25cf7fcf340598c1f6b5',
+    storageBucket: 'angularkanban-e7ae9.appspot.com',
+    apiKey: 'AIzaSyA2zngp5wXot5s18pGJ-oSRg5EooXvkaBk',
+    authDomain: 'angularkanban-e7ae9.firebaseapp.com',
+    messagingSenderId: '302926177904',
+  },
   production: true
 };
